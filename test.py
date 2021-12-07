e0a30eaa970ac9a47186142c69e9b06388ca4ede
Elihu Sela <57669082+elihusela@users.noreply.github.com>
2021-12-07 13:56:50 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import argparse
+
 import torch
 from tqdm import tqdm
+
 import data_loader.data_loaders as module_data
 import model.loss as module_loss
 import model.metric as module_metric
@@ -9,25 +11,21 @@ from parse_config import ConfigParser
 from utils import my_utils
 
 NUM_CLASSES = 1
+RAND_NUM = 0
 
 
 def main(config):
     logger = config.get_logger('test')
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # setup data_loader instances
     data_loader = getattr(module_data, config['data_loader']['type'])(
         config['data_loader']['args']['data_dir'],
-        batch_size=512,
+        batch_size=512,  # 512
         shuffle=False,
         validation_split=0.0,
         training=False,
-        num_workers=2,
-        # My Additions
-        chunk_low_num=config['data_loader']['args']['chunk_low_num'],
-        chunk_high_num=config['data_loader']['args']['chunk_high_num'],
-        partial_change=config['data_loader']['args']['partial_change'],
-        layer_change_lim=config['data_loader']['args']['layer_change_lim']
-
+        num_workers=2
     )
 
     # build model architecture
@@ -39,68 +37,62 @@ def main(config):
     metric_fns = [getattr(module_metric, met) for met in config['metrics']]
 
     logger.info('Loading checkpoint: {} ...'.format(config.resume))
-    # checkpoint = torch.load(config.resume)    #ORIG - dict with state dict in it - not my format
-    # state_dict = checkpoint['state_dict']
-    state_dict = torch.load(config.resume)
+    checkpoint = torch.load(config.resume)
+    state_dict = checkpoint['state_dict']
     if config['n_gpu'] > 1:
         model = torch.nn.DataParallel(model)
     model.load_state_dict(state_dict)
 
     # prepare model for testing
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = model.to(device)
     model.eval()
-    from torchsummary import summary
-
-    summary(model, (1, 121, 11, 21))
 
     total_loss = 0.0
     total_metrics = torch.zeros(len(metric_fns))
 
-    # Accumulate results for histograms
+    """
+    Accumulation of batches - similar to training. Here we take the outputs, targets, bias and also the total number
+    of showers in the batch and the samples indices in the files (was needed for different purposes).
+    """
     tot_bias = torch.Tensor().to(device)
     tot_out = torch.Tensor().to(device)
     tot_target = torch.Tensor().to(device)
-    tot_sums = torch.Tensor().to(device
-                                 )
+    tot_sums = torch.Tensor().to(device)
+    tot_idx = torch.Tensor().to(device)
+
     with torch.no_grad():
         for i, data in enumerate(tqdm(data_loader)):
-            en_dep, x_y, EN = data
-            en_dep, x_y, EN = en_dep.to(device), x_y.to(device), EN.to(device)
+            en_dep, target, num, idx = data
 
-            target = []
-            if NUM_CLASSES == 1:
-                target = EN.float().unsqueeze(1)
-            elif NUM_CLASSES == 2:
-                target = x_y
-            elif NUM_CLASSES == 3:
-                target = torch.cat([x_y, EN], dim=1)
+            ###############################################
+            ### For testing on a totally random dataset ###
+            # for i in range(RAND_NUM):
+            #     en_dep[i] = torch.rand(torch.Size([1, 110, 11, 21]))
+            ###############################################
 
+            en_dep, target, num, idx = en_dep.to(device), target.to(device), num.to(device), idx.to(device)
+            target = target.float()
             output = model(en_dep)
-
-            #
-            # save sample images, or do something with output here
-            #
-
-            # computing loss, metrics on test set
             loss = loss_fn(output, target)
-            bias = target - output
-            tmp_sum = torch.sum(en_dep, dim=[1, 2, 3, -1])  # Sum the entire energy on the ECAL
+            bias = output - target
 
-            # Accum
+            # Accumulate the needed parameters
             tot_bias = torch.cat((tot_bias, bias), 0)
             tot_target = torch.cat((tot_target, target), 0)
             tot_out = torch.cat((tot_out, output), 0)
-            tot_sums = torch.cat((tot_sums, tmp_sum), 0)
+            tot_sums = torch.cat((tot_sums, num), 0)
+            tot_idx = torch.cat((tot_idx, idx), 0)
 
             batch_size = en_dep.shape[0]
             total_loss += loss.item() * batch_size
             for i, metric in enumerate(metric_fns):
                 total_metrics[i] += metric(output, target) * batch_size
 
-    my_utils.plot_and_save_figs(tot_bias, tot_out, tot_target, tot_sums,
-                                config['data_loader']['args']['partial_change'],
-                                config['data_loader']['args']['layer_change_lim'])
+    #################################################################################################
+    ### My evaluation function for generating histograms, images and analysing the tests results ####
+    my_utils.evaluate_test(tot_out.cpu().numpy(), tot_target.cpu().numpy(), tot_idx.cpu().numpy(),
+                           tot_sums.cpu().numpy())
+    #################################################################################################
 
     n_samples = len(data_loader.sampler)
     log = {'loss': total_loss / n_samples}
@@ -121,4 +113,3 @@ if __name__ == '__main__':
 
     config = ConfigParser.from_args(args)
     main(config)
-
